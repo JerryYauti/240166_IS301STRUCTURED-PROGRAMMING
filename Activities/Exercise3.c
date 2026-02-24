f96948ece6adc9241d283859810164b69d7855af
JerryYauti <240166@student.dwu.ac.pg>
2026-02-24 14:55:59 +1000

    Create Exercise3.c
    Program that Print Four Lines of a Favorite Song

---
+++ Activities/Exercise3.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main() 
+{ 
+printf("Line 1: Shine bright like the morning sun\n");
+printf("Line 2: Carry hope until the day is done\n"); 
+printf("Line 3: Walk with courage, never lose your way\n");
+printf("Line 4: Sing with joy, let your spirit sway\n");
+return 0; 
+}
